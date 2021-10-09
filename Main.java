385715f8594f814817f6d0387ffa918ff212ed38
sos-mislom <60609474+sos-mislom@users.noreply.github.com>
2021-10-09 16:41:10 +0500

    финал апдате мейн точка жава!
    реализовал все по тз

--- Main.java
+++ Main.java
@@ -1,33 +1,57 @@
 import java.util.Dictionary;
 import java.math.*;
 import java.util.Hashtable;
+import java.util.Objects;
 import java.util.Scanner;
 
 
 public class Main {
     static Dictionary<String, Integer> list_of_types = new Hashtable<String, Integer>();
+    private static Scanner get_str = new Scanner(System.in);
+    public static String ToBinary(int input_num) {
+        if (input_num > 0){
+            String res = "";
+            int num, index = 0;
+            num = input_num;
 
-    public static String ToBynary(int tmp) {
-        String res = "";
-        int num, k = 0;
-        num = tmp;
-
-        while(tmp > 0) {
-            tmp >>= 1;
-            k ++;
-        }
-
-        while(num > 0) {
-            k -= 1;
-            if (num >= 1 << k) {
-                res += "1";
-                num -= 1 << k;
-            }else {
-                res = res + "0";
+            while(input_num > 0) {
+                input_num >>= 1;
+                index ++;
             }
+
+            while(num > 0) {
+                index -= 1;
+                if (num >= 1 << index) {
+                    res += "1";
+                    num -= 1 << index;
+                }else {
+                    res = res + "0";
+                }
+            }
+            String rep = RepeatStr(index, "0");
+            return String.format("%s%s", res, rep);
+        }else {
+            int n = Math.abs(input_num);
+            String res = String.valueOf(n % 2);
+            while (n >= 2){
+                n = n / 2;
+                res += String.valueOf(n % 2);
+            }
+            return new StringBuilder(res).reverse().toString();
+
         }
-        String rep = RepeatStr(k, "0");
-        return String.format("%s%s", res.toString(), rep);
+
+    }
+    public static String FractionToBin(float frac) {
+        String b = "";
+        int n = 53;
+        while (n > 0) {
+            frac = 2 * frac;
+            b = b + (int)Math.floor(frac);
+            frac = frac - (int)Math.floor(frac);
+            n--;
+        }
+        return b;
     }
     public static String RepeatStr(int i, String str) {
         String result = "";
@@ -36,12 +60,39 @@ public class Main {
         }
         return result;
     }
+    public static String FloatOrDouble(String number, String type_) {
+        int pp=0, mlen=0, b, k;
+        String res="";
+        if (Objects.equals(type_, "float")){
+            pp = 127;
+            mlen = 24;
+        } else if (Objects.equals(type_, "double")){
+            pp = 1023;
+            mlen = 53;}
+        String[] insert_num = (number.split("\\."));
+        String a = ToBinary(Integer.parseInt((insert_num[0])));
+        if (Integer.parseInt((insert_num[0])) < 0){
+            res += "1 ";
+        }
+        else res += "0 ";
+        res += ToBinary(a.length() + pp - 1) + " ";
+        b = 0;
+        k = 10;
+        for (int i = 0; i < insert_num[1].length(); i++) {
+            b += Integer.parseInt(String.valueOf(i)) / k;
+            k *= 10;
+        }
+        a += String.valueOf(FractionToBin(b));
+        res += a.substring(1, mlen) + " ";
+
+        return res;
+    }
     public static String SuperDuperFunc(BigInteger n) {
         int bits = n.bitLength();
         int shift = 1 << bits;
         int mask = shift - 1;
         int number = ((Math.abs(n.intValue()) ^ mask) + 1) & mask;
-        return ToBynary(shift | number);
+        return ToBinary(shift | number);
     }
     public static String RetCoolFormat(String res, String str_, String type_) {
         int coef = res.length();
@@ -78,17 +129,24 @@ public class Main {
         list_of_types.put("short", 2);
         list_of_types.put("int", 4);
         list_of_types.put("long", 8);
-        Scanner get_str = new Scanner(System.in);
-        String first_int = get_str.nextLine();
-        String type_ = get_str.nextLine();
+        list_of_types.put("float", 1);
+        list_of_types.put("double", 1);
+
+        String first_int = input("! Input ur number: \n! ");
+        String type_ = input("! Choose type (byte/short/int/long/float/double): \n! ");
         get_str.close();
-        if(first_int.contains(".") || first_int.contains(",")){
-            System.out.println("in development");
-        }else if (first_int.contains("-")) {
+
+        if(first_int.contains("-")){
             System.out.println(RetCoolFormat(SuperDuperFunc(new BigInteger(first_int)), "1", type_));
+        }else if (first_int.contains(".") || first_int.contains(",") || Objects.equals(type_, "float") || Objects.equals(type_, "double")) {
+            first_int = first_int.replace(',', '.');
+            System.out.println(FloatOrDouble(String.valueOf(Float.parseFloat(first_int)), type_));
         }else{
-            String res = ToBynary(Integer.parseInt(first_int));
-            System.out.println((RetCoolFormat(res,"0", type_)));
+            System.out.println((RetCoolFormat(ToBinary(Integer.parseInt(first_int)),"0", type_)));
         }
+    }
+    static String input(String str_){
+        System.out.print(str_);
+        return get_str.next();
     }
 }
